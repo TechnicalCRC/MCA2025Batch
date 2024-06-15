4b10bd40a3ffc1929b7ad727fad33eef13788ed9
vijayaggerwal <vijay.gniot@gmail.com>
2024-06-15 20:46:11 +0530

    First PHP Content

---
+++ Backend Development using PHP, MySQL/Practice/first.php
@@ -0,0 +1,168 @@
+<?php
+//   echo('hello me');
+
+//  $a = 10;
+//  $c = print('<br>value of a = '.$a);
+//  $b = 20;
+
+//  print '<br>'.$b;
+//  echo '<br>'.$a+$b;
+
+//  $name = 'Ramesh';
+//  echo $name;
+
+//  print '<br>print return value - '.$c;
+//  print '<br>hello';
+//  echo '<br>hello',$a,' bye',$b,' hell',$c,$a,$b;
+
+
+//  print "<br>new line - $a";
+//  print '<br>new line - $a';
+
+//  $sum = $a+$b;
+//  echo "<br> addition of $a & $b is $sum";
+//  echo "<br> addition of $a & $b is $a+$b";
+
+
+// - sin
+# - sin
+/*   - multi */
+
+// $a = 10;
+// echo '<br>'.gettype($a);
+// $a = 'Ravi';
+// echo '<br>'.gettype($a);
+// $a = 10.5;
+// echo '<br>'.gettype($a).'<br><br>';
+
+// $a = 10;
+// echo '<br>'.var_dump($a);
+// $a = 'Ravi';
+// echo '<br>'.var_dump($a);
+// $a = 10.5;
+// echo '<br>'.var_dump($a);
+
+// $ar = [1,2,3];
+// echo '<br>'.var_dump($ar);
+// echo '<br>'.gettype($ar);
+
+
+// $a = '10';
+// echo '<br>'.gettype($a);
+// $b = (integer) $a;
+// echo '<br>'.gettype($b);
+// $c = (string) $b;
+// echo '<br>'.gettype($c);
+
+// $d = NULL;
+// echo '<br>'.gettype($d);
+// echo '<br>'.var_dump($d);
+
+// $a = 13;
+// $b = 30;
+
+// if($a<>$b)
+//  print 'true<br>';
+// else
+//  print 'false<br>';
+
+//  echo '<br>'.($a<=>$b); 
+
+// $a = 10;
+// $b = 20;
+// $a = $a.$b;
+// $a .= $b;
+// echo '<br>'.$a; 
+
+//   $a;
+// print '<br> a = '.$a;
+//  $v_a = $a ?? 20;
+//  print '<br> v_a = '.$v_a;
+
+// if(3==4)
+//  print 'true';
+// else;
+//  echo 'false';
+
+// loop
+
+
+// for($i=0;$i<10;$i++);
+//  print('<br>'.$i); 
+
+//$arr = [11,20,13,4];
+// $arr = ['bba',"bca",'b.com'];
+//  foreach($arr as $i) 
+//  print('<br>'.$i); 
+
+// function
+
+// function show(){
+//   print('Show Me');
+//   echo 'hello me';
+// }
+// show();
+
+// function display($name){
+//   echo '<br>Mr. ' . $name;
+// }
+// display('Varun');
+
+// function sum(){
+//   $a = 100;
+//   $b = 20;
+//   $c = $a + $b;
+//   return $c; 
+// }
+// print ("<br>Sum is ".sum());
+
+// function add($num1,$num2)
+// {
+//   return($num1 + $num2);
+// }
+// echo '<br>add is ' . add(3,4);
+// echo '<br>add is ' . add(13,4);
+// echo '<br>add is ' . add(3,40);
+
+
+
+// Array
+// $arr = [1,2,'hello',3,"Bye",4];
+
+// echo '<br> ' . $arr[2];
+// echo '<br> ' . $arr[0];
+// echo '<br> ' . $arr[3];
+
+// echo '<br> Array Data: <br>';
+// for($i=0;$i<count($arr);$i++){
+//   print('<br>'.$arr[$i]);
+// }
+
+// $cars = ['BMW','Maruti','Tata','KIA',34,"VOLVO"];
+
+// $cars = array('BMW','Maruti','Tata','KIA',34,"VOLVO");
+
+// echo '<br> Cars Data: ';
+
+// foreach($cars as $car){
+//  echo "<br> $car";
+// }
+
+// Associative Array
+
+// $person = array(
+//   "name"=>'Ramesh',
+//   'age'=>45,
+//   'gender'=>'Male'
+// );
+
+// echo 'Name : '.$person['name'];
+// echo '<br> Age : '.$person['age'];
+
+// foreach($person as $k => $v)
+// {
+//   print("<br> $k : $v ");
+// }
+
+echo date('d - m - Y : l  h:i:s a');
+?>
