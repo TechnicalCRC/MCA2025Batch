a3bfca521f022d74bf3e1624327ccd31ec2b9f9b
vijayaggerwal <vijay.gniot@gmail.com>
2024-06-22 14:59:42 +0530

    Insert Record

--- Backend Development using PHP, MySQL/Practice/insert.php
+++ Backend Development using PHP, MySQL/Practice/insert.php
@@ -4,13 +4,18 @@
   <title>Document</title>
 </head>
 <body>
-  <h1>Employee Data</h1>
+  <h1>Insert Employee Data</h1>
+  <hr>
   <form action="insert.php" method="post">
-  Enter Name : <input type="text" name="name" required> <br>
-  Enter Age : <input type="number" name="age" required> <br>
-  Enter Designation : <input type="text" name="designation" required> <br>
-  Enter Salary : <input type="number" name="salary" required> <br>
-  <input type="submit" value="Insert Data">
+  Enter Name : <br>
+  <input type="text" name="name" required style="width:50%"> <br>
+  Enter Age : <br>
+  <input type="number" name="age" style="width:50%" required> <br>
+  Enter Designation : <br>
+  <input type="text" name="designation" style="width:50%" required> <br>
+  Enter Salary : <br>
+  <input type="number" name="salary" style="width:50%" required> <br>
+  <input type="submit" style="width:50%" value="Insert Data">
   </form>  
 
   <?php
@@ -28,6 +33,7 @@
     if($result == true)
         echo "Data Inserted successfully....";
     else
+//       die($con->error);
         echo "Insert Failed....";
    }
 
