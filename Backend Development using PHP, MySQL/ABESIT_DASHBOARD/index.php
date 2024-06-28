09a17a8ed0005193ff0e5c8564fdfb2acc79fcb8
vijayaggerwal <vijay.gniot@gmail.com>
2024-06-28 23:01:58 +0530

    index home page

---
+++ Backend Development using PHP, MySQL/ABESIT_DASHBOARD/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>ABESIT EVENT DASHBOARD</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <?php include 'header.php'; ?>
+  <div class="container-fluid slider mt-2">
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="pic/heck3.jpeg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="pic/heck5.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="pic/sih22.png" class="d-block w-100" alt="...">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <h1 class="text-center m-4" id="heading">About Us</h1>
+    <div class="row">
+      <div class="col-md-4">
+        <img src="pic/heck5.jpg" alt="" style='width:90%'>
+      </div>
+      <div class="col-md-8">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quo. Molestias magni animi consequatur facilis. Cum officiis vitae dolorem, laborum dolor repellat debitis in numquam, quae fuga sed asperiores nisi? Corrupti, cupiditate earum, soluta vitae quam similique officiis doloribus deserunt omnis, quo veritatis ullam at ipsam qui neque quis harum id dicta. Corporis sequi at earum fuga ullam, ipsa laborum odio dolorem rem dolor, consequatur, quibusdam veritatis cum maiores libero officia! Eligendi est consequuntur commodi recusandae eos laudantium assumenda natus, aut error perferendis. Beatae eveniet deserunt dolor ut neque laborum, atque voluptatum hic ratione. Beatae, facere praesentium tempora error quas amet, eveniet ad sint nihil libero necessitatibus quis iste blanditiis illum suscipit nesciunt dolores accusamus, nobis voluptatum est impedit officiis aut? Error, saepe optio omnis repudiandae, debitis, recusandae dolore numquam autem alias eligendi ex molestiae eveniet blanditiis impedit minima quod architecto reprehenderit nam quam ipsam doloribus ipsum. Impedit, ab voluptates?</p>
+      </div>
+    </div>
+   <hr class="border border-dark border-2 opacity-50">
+  </div>
+
+  <div class="container">
+  <h1 class="text-center m-4" id="heading">My Events</h1>
+    
+  </div>
+  
+
+</body>
+
+</html>
