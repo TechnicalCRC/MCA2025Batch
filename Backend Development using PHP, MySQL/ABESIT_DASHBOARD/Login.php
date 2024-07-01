b60de7bee21493502641528aaadb33ace8583522
vijayaggerwal <vijay.gniot@gmail.com>
2024-07-01 11:06:37 +0530

    Login

--- Backend Development using PHP, MySQL/ABESIT_DASHBOARD/Login.php
+++ Backend Development using PHP, MySQL/ABESIT_DASHBOARD/Login.php
@@ -5,10 +5,11 @@
   <title>Login Page</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
+  <?php include 'header.php'; ?>
   <div class="container">
     <h1 class="text-center">Login Page</h1>
     <?php
@@ -63,9 +64,12 @@ else{
       </div>
       <button type="submit" class="btn btn-primary">Sign In</button>
     </form>
+    <p> Not Register Yet? Kindly <a href="Registration.php">register here</a> </p>
 
 
   </div>
+  <?php include 'footer.php'; ?>
+
 
 </body>
 
