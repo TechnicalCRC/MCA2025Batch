ecedaa3bcf3f7007ada7c6324a5a7c1f0e2751e9
vijayaggerwal <vijay.gniot@gmail.com>
2024-07-01 11:07:39 +0530

    dashboard

--- Backend Development using PHP, MySQL/ABESIT_DASHBOARD/Dashboard/Event_Dashboard.php
+++ Backend Development using PHP, MySQL/ABESIT_DASHBOARD/Dashboard/Event_Dashboard.php
@@ -3,8 +3,27 @@
  if(!isset($_SESSION['username'])){
   header('location:../Login.php');
  }
- echo 'Welcome Mr. ' . $_SESSION['username'];
-
+// echo 'Welcome Mr. ' . $_SESSION['username'];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+  <?php include 'header.php'; ?>
+
 <br><br><br>
 <a href="../Logout.php">Logout</a>
+
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
+
+
+
